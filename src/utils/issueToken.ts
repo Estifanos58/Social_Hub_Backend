abc86b77627c21898b6439d91d15c7b1ff2e8fde
Estifanos58 <estifkebe08@gmail.com>
2025-10-03 10:07:44 +0300

    update(token): access and refresh token time increased

--- src/utils/issueToken.ts
+++ src/utils/issueToken.ts
@@ -3,17 +3,20 @@ import { JwtService } from "@nestjs/jwt";
 import { User } from "@prisma/client";
 import { Response } from "express";
 
+export const ACCESS_TOKEN_EXPIRES_AT = '1hr'
+export const REFRESH_TOKEN_EXPRIRES_AT = '15d'
+
 export const issueToken = (user: User, jwtService: JwtService, configService: ConfigService, response: Response) => {
     const payload = { sub: user.id, email: user.email, firstname: user.firstname };
 
     const accessToken = jwtService.sign(payload, {
         secret: configService.get<string>('ACCESS_TOKEN_SECRET'),
-        expiresIn: '15m',
+        expiresIn: ACCESS_TOKEN_EXPIRES_AT,
     });
 
     const refreshToken = jwtService.sign(payload, {
         secret: configService.get<string>('REFRESH_TOKEN_SECRET'),
-        expiresIn: '7d',
+        expiresIn: REFRESH_TOKEN_EXPRIRES_AT,
     });
 
     response.cookie('refreshToken', refreshToken, { httpOnly: true })
